aa5078bf99c97dd27d7d20a243dbd5dcd214245a
Adrjanjan <adrjanjan@gmail.com>
2017-11-16 16:14:25 +0100

    message

---
+++ 1.c
@@ -0,0 +1,64 @@
+// Minimalne drzewo rozpinaj¹ce
+// Algorytm Kruskala
+// Data: 6.04.2014
+// (C)2014 mgr Jerzy Wa³aszek
+//--------------------------------
+
+// Definicja obiektu kolejki priorytetowej
+//----------------------------------------
+type
+  Edge = record
+    v1,v2  : integer;             // Wierzcho³ki krawêdzi
+    weight : integer;             // Waga krawêdzi
+  end;
+
+  Queue = object
+    private
+      Heap : array of Edge;
+      hpos : integer;
+    public
+      constructor init(n : integer);
+      destructor  destroy;
+      function    front : Edge;
+      procedure   push(e : Edge);
+      procedure   pop;
+  end;
+
+// Definicja obiektu struktury zbiorów roz³¹cznych
+//------------------------------------------------
+  DSNode = record
+    up   : integer;
+    rank : integer;
+  end;
+
+  DSStruct = object
+    private
+      Z : array of DSNode;
+    public
+      constructor init(n : integer);
+      destructor  destroy;
+      procedure   MakeSet(v : integer);
+      function    FindSet(v : integer) : integer;
+      procedure   UnionSets(e : Edge);
+  end;
+
+// Definicja obiektu minimalnego drzewa rozpinaj¹cego
+//---------------------------------------------------
+  PTNode = ^TNode;
+  TNode = record
+    next   : PTNode;
+    v      : integer;
+    weight : integer;
+  end;
+
+  MSTree = object
+    private
+      A : array of PTNode;        // Tablica list s¹siedztwa
+      Alen : integer;             // Liczba komórek w tablicy
+      weight : integer;           // Waga ca³ego drzewa
+    public
+      constructor init(n : integer);
+      destructor  destroy;
+      procedure   addEdge(e : Edge);
+      procedure   print;
+  end;
