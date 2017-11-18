a7944d29c8b25eeb7550a5743e3f034acdcb7dff
notnamenot <33725887+notnamenot@users.noreply.github.com>
2017-11-18 19:27:30 +0100

    Update 2.c
    change2

--- 2.c
+++ 2.c
@@ -1,6 +1,6 @@
 program eulercp;
 
-// Typy dla dynamicznej tablicy list s¹siedztwa oraz stosu
+// Typy dla dynamicznej tablicy list sÂ¹siedztwa oraz stosu
 type
   PslistEl = ^slistEl;
   slistEl =  record
@@ -8,13 +8,17 @@ type
     v     : integer;
   end;
 
+
+
+
+
 TList = array of PslistEl;
 
 // Definicja typu obiektowego stack
 //---------------------------------
 stack = object
   private
-    S : PslistEl;  // lista przechowuj¹ca stos
+    S : PslistEl;  // lista przechowujÂ¹ca stos
 
   public
     constructor init;
@@ -47,7 +51,7 @@ begin
   if S = nil then empty := true else empty := false;
 end;
 
-// Zwraca liczbê ze szczytu stosu
+// Zwraca liczbÃª ze szczytu stosu
 //----------------------------------
 function stack.top : integer;
 begin
